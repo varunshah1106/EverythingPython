8285766beabfb0369d1e9f6715d1c28cc64968d9
Varun Shah <varunshah1106@varuns-mbp.attlocal.net>
2018-03-14 00:51:28 -0500

    1. Fixing README 2. Adding randomized_quicksort
    Signed-off-by: Varun Shah <varunshah1106@varuns-mbp.attlocal.net>

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,5 +1,6 @@
 from sorts.quicksort import quicksort
 from sorts.mergesort import mergesort
+from sorts.randomized_quicksort import randomized_quicksort
 import unittest
 
 
@@ -22,5 +23,14 @@ class TestMergesort(unittest.TestCase):
         self.assertEqual(mergesort([]), [])
         self.assertEqual(mergesort([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5])
 
+
+class TestRandomizedQuicksort(unittest.TestCase):
+    def test(self):
+        self.assertEqual(randomized_quicksort([5, 5, 6, 123, 5464, 2, 0, -1]), [-1, 0, 2, 5, 5, 6, 123, 5464])
+        self.assertEqual(randomized_quicksort([5, 6]), [5, 6])
+        self.assertEqual(randomized_quicksort([5]), [5])
+        self.assertEqual(randomized_quicksort([6, 5]), [5, 6])
+        self.assertEqual(randomized_quicksort([]), [])
+        self.assertEqual(randomized_quicksort([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5])
 
 unittest.main(exit = False)
