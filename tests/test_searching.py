853292e2b6190583fba7ae798af26c3c7535a9a3
Varun Shah <varunshah1106@varuns-mbp.attlocal.net>
2018-03-15 00:39:01 -0500

    Add linear search
    Signed-off-by: Varun Shah <varunshah1106@varuns-mbp.attlocal.net>

--- tests/test_searching.py
+++ tests/test_searching.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestLinearSearch(unittest.TestCase):
     def test(self):
-        self.assertEqual(linear_search([1, 2, 3, 4, 5], 3), 3)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 3), 2)
         self.assertEqual(linear_search([1, 2, 3, 4, 5], 0), -1)
         self.assertEqual(linear_search([], 3), -1)
         self.assertEqual(linear_search([1, 2, 3, 4, 5], None), -1)
