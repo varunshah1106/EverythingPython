e07f1593b035824814aa7a49d47e0297b18ffb02
Varun Shah <varunshah1106@varuns-mbp.attlocal.net>
2018-03-15 00:34:33 -0500

    Added search tests to sort file, oops
    Signed-off-by: Varun Shah <varunshah1106@varuns-mbp.attlocal.net>

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,7 +1,6 @@
 from sorts.quicksort import quicksort
 from sorts.mergesort import mergesort
 from sorts.randomized_quicksort import randomized_quicksort
-from searching.linear_search import linear_search
 
 import unittest
 
@@ -34,11 +33,3 @@ class TestRandomizedQuicksort(unittest.TestCase):
         self.assertEqual(randomized_quicksort([6, 5]), [5, 6])
         self.assertEqual(randomized_quicksort([]), [])
         self.assertEqual(randomized_quicksort([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5])
-
-
-class TestLinearSearch(unittest.TestCase):
-    def test(self):
-        self.assertEqual(linear_search([1, 2, 3, 4, 5], 3), 3)
-        self.assertEqual(linear_search([1, 2, 3, 4, 5], 0), -1)
-        self.assertEqual(linear_search([], 3), -1)
-        self.assertEqual(linear_search([1, 2, 3, 4, 5], None), -1)
