18162304563b14afecdc675b490a1bdad0e3baf3
Varun Shah <varunshah1106@varuns-mbp.attlocal.net>
2018-04-07 05:25:01 -0500

    Adding DFS and BFS

--- tests/graph/test_graph.py
+++ tests/graph/test_graph.py
@@ -199,6 +199,25 @@ class TestUndirectedGraph(unittest.TestCase):
         self.assertFalse(graph.is_connected('a', 'b'))
         self.assertFalse(graph.is_connected('a', 'c'))
 
+    def test_topological_sort(self):
+        graph = Graph(directed=False)
+        graph.add_edge(data1='a', data2=123, weight=3)
+        graph.add_edge(data1='b', data2=123, weight=2)
+        graph.add_edge(data1='c', data2=123, weight=1)
+        graph.add_edge(data1='c', data2='a', weight=1)
+        with self.assertRaises(TypeError) as error:
+            t = graph.topological_sort()
+
+    def test_dfs(self):
+        graph = Graph(directed=False)
+        graph.add_edge(data1='a', data2=123, weight=3)
+        graph.add_edge(data1='b', data2=123, weight=2)
+        graph.add_edge(data1='c', data2=123, weight=1)
+        graph.add_edge(data1='c', data2='a', weight=1)
+        with self.assertRaises(TypeError) as error:
+            t = graph.dfs(node='c')
+
+
 class TestDirectedGraph(unittest.TestCase):
 
     def test_graph_initialization(self):
@@ -220,18 +239,74 @@ class TestDirectedGraph(unittest.TestCase):
         graph.add_edge(data1='a', data2=123, weight=3)
         self.assertEqual(graph.adjacency_list['a'].adjacent[123], 3)
         self.assertEqual(graph.adjacency_list[123].adjacent, {})
-        self.assertEqual(graph.adjacency_list[123].in_degree, 1)
-        self.assertEqual(graph.adjacency_list['a'].in_degree, 0)
 
     def test_remove_edge(self):
         graph = Graph(directed=True)
         graph.add_edge(data1='a', data2=123, weight=3)
-        graph.add_edge(data1='b', data2=123, weight=3)
-        graph.add_edge(data1='c', data2=123, weight=3)
-        self.assertEqual(graph.adjacency_list[123].in_degree, 3)
+        graph.add_edge(data1='b', data2=123, weight=2)
+        graph.add_edge(data1='c', data2=123, weight=1)
+        self.assertEqual(graph.adjacency_list['a'].adjacent[123], 3)
+        self.assertEqual(graph.adjacency_list['b'].adjacent[123], 2)
+        self.assertEqual(graph.adjacency_list['c'].adjacent[123], 1)
         graph.remove_edge('a', 123)
-        self.assertEqual(graph.adjacency_list[123].in_degree, 2)
+        self.assertEqual(graph.adjacency_list['a'].adjacent, {})
         graph.remove_edge('b', 123)
-        self.assertEqual(graph.adjacency_list[123].in_degree, 1)
+        self.assertEqual(graph.adjacency_list['b'].adjacent, {})
         graph.remove_edge('c', 123)
-        self.assertEqual(graph.adjacency_list[123].in_degree, 0)
+        self.assertEqual(graph.adjacency_list['c'].adjacent, {})
+
+    def test_topological_sort(self):
+        graph = Graph(directed=True)
+        graph.add_edge(data1='a', data2=123, weight=3)
+        graph.add_edge(data1='b', data2=123, weight=2)
+        graph.add_edge(data1='c', data2=123, weight=1)
+        graph.add_edge(data1='c', data2='a', weight=1)
+        t = graph.topological_sort()
+        self.assertEqual(t.index(123), 3)
+
+    def test_topological_sort_empty_graph(self):
+        graph = Graph(directed=True)
+        t = graph.topological_sort()
+        self.assertEqual(t, [])
+
+    def test_dfs(self):
+        graph = Graph(directed=True)
+        graph.add_edge(data1='a', data2='b')
+        graph.add_edge(data1='a', data2='c')
+        graph.add_edge(data1='b', data2='d')
+        graph.add_edge(data1='c', data2='e')
+        t = graph.dfs(node='a')
+        self.assertNotEqual(t.index('b'), 1)
+        self.assertNotEqual(t.index('b'), 3)
+        self.assertNotEqual(t.index('c'), 1)
+        self.assertNotEqual(t.index('c'), 3)
+        self.assertNotEqual(t.index('d'), 0)
+        self.assertNotEqual(t.index('d'), 2)
+        self.assertNotEqual(t.index('e'), 0)
+        self.assertNotEqual(t.index('e'), 2)
+
+    def test_dfs_empty_graph(self):
+        graph = Graph(directed=True)
+        t = graph.dfs(node='c')
+        self.assertIsNone(t)
+
+    def test_bfs(self):
+        graph = Graph(directed=True)
+        graph.add_edge(data1='a', data2='b')
+        graph.add_edge(data1='a', data2='c')
+        graph.add_edge(data1='b', data2='d')
+        graph.add_edge(data1='c', data2='e')
+        t = graph.bfs(node='a')
+        self.assertNotEqual(t.index('b'), 2)
+        self.assertNotEqual(t.index('b'), 3)
+        self.assertNotEqual(t.index('c'), 2)
+        self.assertNotEqual(t.index('c'), 3)
+        self.assertNotEqual(t.index('d'), 0)
+        self.assertNotEqual(t.index('d'), 1)
+        self.assertNotEqual(t.index('e'), 0)
+        self.assertNotEqual(t.index('e'), 1)
+
+    def test_bfs_empty_graph(self):
+        graph = Graph(directed=True)
+        t = graph.bfs(node='c')
+        self.assertIsNone(t)
